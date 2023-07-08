6156a99bcdb4c8b7a1d883936a2d97ffe70d1264
yooonchul <yoonchulshin610@gmail.com>
2023-07-07 22:41:55 -0400

    more api!

--- app/google_api.py
+++ app/google_api.py
@@ -7,11 +7,38 @@ url = f'https://www.googleapis.com/civicinfo/v2/representatives?address={address
 
 response = requests.get(url)
 
-print('Status code:', response.status_code)
+#print('Status code:', response.status_code)
 
 data = response.json()
+def get_reps():
+    for office in data['offices']:
+        for official_index in office['officialIndices']:
+            official = data['officials'][official_index]
+            print(office['name'], official['name'])
 
-for office in data['offices']:
-    for official_index in office['officialIndices']:
-        official = data['officials'][official_index]
-        print(office['name'], official['name'])
+base_url = "https://www.googleapis.com/civicinfo/v2/voterinfo"
+def get_voter_info(address):
+    params = {
+        "address": address,
+        "key": 'AIzaSyDX7fJhgMw6P86Mh1WxprExe2rOdEM5bSU'
+    }
+
+    response = requests.get(base_url, params=params)
+    response_data = response.json()
+    return response_data
+
+voter_info = get_voter_info(address)
+print(voter_info)
+
+z = "https://www.googleapis.com/civicinfo/v2/elections"
+
+def get_elections():
+    params = {
+        "key" : 'AIzaSyDX7fJhgMw6P86Mh1WxprExe2rOdEM5bSU'
+    }
+    response = requests.get(z, params = params)
+    abc = response.json()
+    return abc
+
+
+print(get_elections())
