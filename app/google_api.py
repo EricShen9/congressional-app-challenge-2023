f04a0468b8639db6b5777310eec8c4dfc7d15350
yooonchul <yoonchulshin610@gmail.com>
2023-07-07 22:13:43 -0400

    z

--- app/google_api.py
+++ app/google_api.py
@@ -1,10 +1,9 @@
 import requests
 import json
 
-api_key = 'AIzaSyDX7fJhgMw6P86Mh1WxprExe2rOdEM5bSU'
 address = "1523 Beacon St Waban MA"
 
-url = f'https://www.googleapis.com/civicinfo/v2/representatives?address={address}&key={api_key}'
+url = f'https://www.googleapis.com/civicinfo/v2/representatives?address={address}&key=AIzaSyDX7fJhgMw6P86Mh1WxprExe2rOdEM5bSU'
 
 response = requests.get(url)
 
