a96b3a0bcff3e8e5c389dedad2489dc475f4ffc8
yooonchul <yoonchulshin610@gmail.com>
2023-07-07 23:28:38 -0400

    my glorious king!

--- app/page.js
+++ app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/lebron.jpg"
+              alt="my glorious king"
               className={styles.vercelLogo}
               width={100}
               height={24}
